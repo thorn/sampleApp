0ec6ce4c0fb155075e2efa90e7701ed026687e90
Arsen <notdefined@list.ru>
2011-07-29 10:44:43 +0400

    Defined title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # Title helper
+  def title
+    base_title = "My app"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} | #{@title}"
+    end
+  end
 end
